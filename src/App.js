c5f397d9bd2c200f931084b0e2d67cdaf7ef538d
AceOfHeart5 <evanconwaymusic@gmail.com>
2020-09-24 01:16:15 -0400

    add some modal logic

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
 	return (
 		<div className="App">
 			<button onClick={openModal}>Open Modal</button>
-			<Modal/>
+			<Modal>
+				<h1>Modal Header</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dolor ab asperiores enim dolore omnis similique veritatis numquam, porro in deleniti voluptatum amet, deserunt eveniet sequi error eligendi dignissimos corporis?</p>
+			</Modal>
 		</div>
 	);
 }
--- src/Modal.js
+++ src/Modal.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Modal = (props) => {
-    return(
-        <div className="modal-wrapper">
-            <div className="modal-backdrop"></div>
-            <div className="modal-box">
-                {props.children}
-            </div>
-        </div>
-    )
+class Modal extends Component {
+    state = { display: true };
+
+    open = () => {
+        this.setState({
+            display: true
+        })
+    };
+
+    close = () => {
+        this.setState({
+            display: false
+        })
+    }
+
+    render() {
+        if (this.state.display) {
+            return (
+                <div className="modal-wrapper">
+                    <div onClick={this.close} className="modal-backdrop"></div>
+                    <div className="modal-box">
+                        {this.props.children}
+                    </div>
+                </div>
+            )
+        } else {
+            return null;
+        }
+    }
 }
 
 export default Modal;
