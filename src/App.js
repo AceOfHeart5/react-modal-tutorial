b56ae0bef176f0a756a6b4eec40f5766383fa7e1
AceOfHeart5 <evanconwaymusic@gmail.com>
2020-09-23 23:09:01 -0400

    add modal component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Modal from './Modal';
+
+const openModal = () => {
+	console.log("Open Modal");
+}
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return (
+		<div className="App">
+			<button onClick={openModal}>Open Modal</button>
+			<Modal/>
+		</div>
+	);
 }
 
 export default App;
